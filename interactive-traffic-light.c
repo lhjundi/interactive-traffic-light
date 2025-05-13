e3106f9be0bb50cc5451dd35e7cb7674a6296de1
Lucas Jundi Hikazudani <lhjundi@outlook.com>
2025-05-13 17:32:01 -0300

    feat: add interrupt request

--- interactive-traffic-light.c
+++ interactive-traffic-light.c
@@ -5,7 +5,7 @@
 
 #define GREEN_LED 11
 #define RED_LED 13
-
+#define BUTTON 5
 
 void turn_on_red_signal();
 void turn_on_yellow_signal();
@@ -16,7 +16,16 @@ int64_t turn_on_yellow_callback(alarm_id_t id, void *user_data);
 int64_t turn_on_green_callback(alarm_id_t id, void *user_data);
 int64_t turn_on_red_callback(alarm_id_t id, void *user_data);
 void setup();
+void button_interrupt_handler(uint gpio, uint32_t events);
 
+void button_interrupt_handler(uint gpio, uint32_t events)
+{
+    if (events & GPIO_IRQ_EDGE_FALL)
+    {
+        printf("Pedestrian button!\n");
+        turn_on_yellow_signal();
+    }
+}
 
 void turn_on_green_signal()
 {
@@ -64,7 +73,6 @@ void setup()
 {
     stdio_init_all();
 
-    // Inicializa LEDs
     gpio_init(GREEN_LED);
     gpio_set_dir(GREEN_LED, GPIO_OUT);
     gpio_put(GREEN_LED, 0);
@@ -72,6 +80,10 @@ void setup()
     gpio_init(RED_LED);
     gpio_set_dir(RED_LED, GPIO_OUT);
     gpio_put(RED_LED, 0);
+
+    gpio_init(BUTTON);
+    gpio_set_dir(BUTTON, GPIO_IN);
+    gpio_pull_up(BUTTON);
 }
 
 int main()
@@ -80,6 +92,7 @@ int main()
     
     printf("Traffic Light System\n");
 
+    gpio_set_irq_enabled_with_callback(BUTTON, GPIO_IRQ_EDGE_FALL, true, &button_interrupt_handler);
     add_alarm_in_ms(2000, turn_on_red_callback, NULL, false);
 
     while (true)
