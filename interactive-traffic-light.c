9ee3148d6c78719231fa31fc7c6f581b1800d730
Lucas Jundi Hikazudani <lhjundi@outlook.com>
2025-05-17 13:28:00 -0300

    feat: add countdown when a button is pressed

--- interactive-traffic-light.c
+++ interactive-traffic-light.c
@@ -12,7 +12,8 @@
 #define BUZZER 21
 #define BUZZER_FREQ 100
 
-typedef enum{
+typedef enum
+{
     RED,
     YELLOW,
     GREEN
@@ -25,7 +26,8 @@ struct light_state
 };
 
 volatile struct light_state current = {RED, 10000};
-volatile bool button_pressed = false;
+volatile bool button_A_pressed = false;
+volatile bool button_B_pressed = false;
 
 // Function prototypes
 void turn_on_red_signal();
@@ -34,7 +36,7 @@ void turn_on_green_signal();
 void turn_on_red_signal();
 void turn_on_yellow_signal();
 void setup();
-void BUTTON_A_interrupt_handler(uint gpio, uint32_t events);
+void button_interrupt_handler(uint gpio, uint32_t events);
 void change_state();
 void pwm_init_buzzer(uint pin);
 void beep(uint pin, uint32_t duration_ms);
@@ -42,20 +44,21 @@ int64_t beep_stop_callback(alarm_id_t id, void *user_data);
 bool state_controller();
 bool is_time_to_change();
 
-int64_t beep_stop_callback(alarm_id_t id, void *user_data) {
+int64_t beep_stop_callback(alarm_id_t id, void *user_data)
+{
     uint pin = (uintptr_t)user_data;
     pwm_set_gpio_level(pin, 0);
     return 0;
 }
 
-void beep(uint pin, uint32_t duration_ms) {
+void beep(uint pin, uint32_t duration_ms)
+{
     // Liga o buzzer com duty cycle de 50%
     pwm_set_gpio_level(pin, 2048);
 
     // Passa o pino como ponteiro para desligar depois
-    add_alarm_in_ms(duration_ms, beep_stop_callback, (void*)(uintptr_t)pin, false);
+    add_alarm_in_ms(duration_ms, beep_stop_callback, (void *)(uintptr_t)pin, false);
 }
-
 
 void pwm_init_buzzer(uint pin)
 {
@@ -67,55 +70,60 @@ void pwm_init_buzzer(uint pin)
     pwm_set_gpio_level(pin, 0);
 }
 
-bool state_controller(){
-    printf("Duration: %d seconds\n", current.duration/1000);
+bool state_controller()
+{
     current.duration -= 1000;
+    if (current.state == RED && current.duration <= 5000 && (button_A_pressed || button_B_pressed))
+        printf("Duration: %d seconds\n", current.duration / 1000);
 
-    if (button_pressed && current.state == RED && current.duration == 5000)
+    if ((button_A_pressed || button_B_pressed) && current.state == RED && current.duration == 5000)
     {
         beep(BUZZER, 5000);
-        button_pressed = false;
     }
-    if(is_time_to_change())
+    if (is_time_to_change())
         change_state();
     return true;
 }
 
 bool is_time_to_change()
 {
-    if (current.duration <= 0) return true;
+    if (current.duration <= 0)
+        return true;
     return false;
 }
 
-void change_state(){
+void change_state()
+{
     switch (current.state)
     {
-        case RED:
-            turn_on_green_signal();
-            current.state = GREEN;
-            current.duration = 10000;
-            break;
-        case GREEN:
-            turn_on_yellow_signal();
-            current.state = YELLOW;
-            current.duration = 3000;
-            break;
-        case YELLOW:
-            turn_on_red_signal();
-            current.state = RED;
-            current.duration = 10000;
-            break;
+    case RED:
+        button_A_pressed = false;
+        button_B_pressed = false;
+        turn_on_green_signal();
+        current.state = GREEN;
+        current.duration = 10000;
+        break;
+    case GREEN:
+        turn_on_yellow_signal();
+        current.state = YELLOW;
+        current.duration = 3000;
+        break;
+    case YELLOW:
+        turn_on_red_signal();
+        current.state = RED;
+        current.duration = 10000;
+        break;
     }
 }
 
-void BUTTON_A_interrupt_handler(uint gpio, uint32_t events)
+void button_interrupt_handler(uint gpio, uint32_t events)
 {
     if (events & GPIO_IRQ_EDGE_FALL)
     {
-        printf("Pedestrian BUTTON_A activated!\n");
+        printf("Pedestrian button %c activated!\n", gpio == BUTTON_A ? 'A' : 'B');
         current.duration = 1000;
         current.state = GREEN;
-        button_pressed = true;
+        button_A_pressed = true;
     }
 }
 
@@ -155,6 +163,11 @@ void setup()
     gpio_init(BUTTON_A);
     gpio_set_dir(BUTTON_A, GPIO_IN);
     gpio_pull_up(BUTTON_A);
+
+    gpio_init(BUTTON_B);
+    gpio_set_dir(BUTTON_B, GPIO_IN);
+    gpio_pull_up(BUTTON_B);
+
     pwm_init_buzzer(BUZZER);
     sleep_ms(2000);
     printf("Traffic Light System\n");
@@ -169,9 +182,9 @@ int main()
     struct repeating_timer buzzer_timer;
 
     add_repeating_timer_ms(-1000, state_controller, NULL, &timer);
-    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &BUTTON_A_interrupt_handler);
-    // add_alarm_in_ms(1000, state_controller, NULL, true);
-    
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &button_interrupt_handler);
+    gpio_set_irq_enabled(BUTTON_B, GPIO_IRQ_EDGE_FALL, true);
+
     while (true)
     {
         tight_loop_contents();
